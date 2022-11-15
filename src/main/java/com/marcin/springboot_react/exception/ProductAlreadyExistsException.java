a1a565b2ac41e002dd652d51595a8a4c3ab8e22c
Marcin Nieweglowski WorkAdmin <mnieweglowski_wa@objectivity.co.uk>
2022-11-15 15:47:28 +0100

    adding response http statuses to exceptions

--- src/main/java/com/marcin/springboot_react/exception/ProductAlreadyExistsException.java
+++ src/main/java/com/marcin/springboot_react/exception/ProductAlreadyExistsException.java
@@ -1,5 +1,9 @@
 package com.marcin.springboot_react.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT)
 public class ProductAlreadyExistsException extends RuntimeException {
 
     public ProductAlreadyExistsException(String message) {
